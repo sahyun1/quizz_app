ab6089df6e78ecb284a97612025280749408bc41
Alston Sahyun Kim <sahyun1@hotmail.com>
2021-06-27 19:13:22 +1200

    added question.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quizz_app/question.dart';
 
+import './question.dart';
 // void main() {
 //   runApp(MyApp());
 // }
@@ -34,7 +36,7 @@ class _MyAppState extends State<MyApp> {
       ),
       body: Column(
         children: [
-          Text(questions[_questionIndex]),
+          Question(questions[_questionIndex]),
           RaisedButton(
             child: Text('Answer1'),
             onPressed: answerQuestion,
